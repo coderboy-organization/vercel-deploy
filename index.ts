3ed89d509c41266cfd616088619b4af5f6236f08
devlopersabbir <devlopersabbir@gmail.com>
2023-12-18 00:04:37 +0600

    init

---
+++ index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello World" });
+});
+export default app;
